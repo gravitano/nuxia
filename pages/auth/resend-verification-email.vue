b2b5ea06045f5eadef2d2685750ccfbc961d8bda
Warsono <warsono16694@gmail.com>
2025-04-23 21:46:17 +0700

    feat(auth): create resend verification email page with appropriate metadata

---
+++ pages/auth/resend-verification-email.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+useHead({
+  title: "Resend Verification Email",
+  meta: [
+    {
+      name: "description",
+      content: "Resend Verification Email",
+    },
+  ],
+});
+
+definePageMeta({
+  layout: "auth",
+  middleware: "guest",
+});
+</script>
+
+<template>
+  <AuthForgotPasswordForm />
+</template>
