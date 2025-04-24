0e935a829091361f9f777534456adf3987d76b3d
Warsono <warsono16694@gmail.com>
2025-04-24 10:40:18 +0700

    fix(settings): update hover background color for menu items

--- components/settings/SettingsMenu.vue
+++ components/settings/SettingsMenu.vue
@@ -20,7 +20,7 @@ const menus = [
     <li v-for="menu in menus" :key="menu.label">
       <NuxtLink
         :to="menu.to"
-        class="w-full flex items-center px-3 py-2 rounded-md hover:bg-neutral-100 dark:hover:bg-neutral-900 text-sm font-medium"
+        class="w-full flex items-center px-3 py-2 rounded-md hover:bg-sidebar-accent dark:hover:bg-neutral-900 text-sm font-medium"
         exact-active-class="bg-neutral-100 dark:bg-neutral-900 text-neutral-900 dark:text-neutral-100"
       >
         {{ menu.label }}
