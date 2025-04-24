faaaf464f71540e1d420d07356ce9aa85021f133
Warsono <warsono16694@gmail.com>
2025-04-24 10:23:38 +0700

    feat(account): implement account deletion functionality with validation and loading state

--- components/settings/DeleteAccountDialog.vue
+++ components/settings/DeleteAccountDialog.vue
@@ -19,25 +19,46 @@ import {
   FormControl,
   FormField,
   FormItem,
-  FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { deleteAccountSchema } from "~/shared/shemas/delete-account";
+import { Loader } from "lucide-vue-next";
 
 const isOpen = defineModel<boolean>("open");
 
-const formSchema = z.object({
-  password: z.string(),
-});
-
 const { handleSubmit, isSubmitting, meta } = useForm({
-  validationSchema: toTypedSchema(formSchema),
+  validationSchema: toTypedSchema(deleteAccountSchema),
 });
 
-const onSubmit = handleSubmit((_values) => {
-  toast.success("Account deleted successfully");
-  isOpen.value = false;
-  navigateTo("/auth/login");
+const { clear } = useUserSession();
+
+const isLoading = ref(false);
+
+const onSubmit = handleSubmit((values) => {
+  isLoading.value = true;
+
+  $fetch("/api/settings/account", {
+    method: "DELETE",
+    body: JSON.stringify(values),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then(() => {
+      toast.success("Account deleted successfully");
+      isOpen.value = false;
+      clear();
+      navigateTo("/auth/login");
+    })
+    .catch((error) => {
+      toast.error(
+        error.data?.message || "An error occurred while deleting your account"
+      );
+    })
+    .finally(() => {
+      isLoading.value = false;
+    });
 });
 </script>
 
@@ -78,11 +99,20 @@ const onSubmit = handleSubmit((_values) => {
           <Button
             variant="destructive"
             type="submit"
-            :disabled="isSubmitting || !meta.valid"
+            :disabled="isSubmitting || !meta.valid || isLoading"
           >
+            <Loader
+              v-if="isSubmitting || isLoading"
+              class="mr-2 h-4 w-4 animate-spin"
+            />
             Delete account
           </Button>
-          <Button type="button" variant="outline" @click="isOpen = false">
+          <Button
+            type="button"
+            variant="outline"
+            :disabled="isSubmitting || isLoading"
+            @click="isOpen = false"
+          >
             Cancel
           </Button>
         </DialogFooter>
